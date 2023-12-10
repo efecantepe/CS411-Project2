24039e070508e8393049268efef02fa4c1907557
BarisYildirim6 <byildirim2561@gmail.com>
2023-12-10 12:41:12 +0300

    MUI components && router-dom dependencies added

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import BookmarkRemoveIcon from '@mui/icons-material/BookmarkRemove';
+import BookmarksIcon from '@mui/icons-material/Bookmarks';
+import { Box, Grid } from '@mui/material';
+
+const Home = () => {
+	return <Grid container spacing={2}></Grid>;
+};
+export default Home;
